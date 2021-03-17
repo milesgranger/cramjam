5ce7bbdd314261058fb9abe66be709758b1d88b0
milesgranger <miles59923@gmail.com>
2021-03-17 22:58:43 +0100

    chkpt: working on docs

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -2,5 +2,8 @@
 use pyo3::create_exception;
 use pyo3::exceptions::PyException;
 
+/// Raised during any error which occurs during compression
 create_exception!(cramjam, CompressionError, PyException);
+
+/// Raised during any error which occurs during decompression
 create_exception!(cramjam, DecompressionError, PyException);
