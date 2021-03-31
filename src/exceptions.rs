b0404f46d71abdc612d688bf001ee86b15dca23c
milesgranger <miles59923@gmail.com>
2021-03-31 10:18:41 +0200

    Use OsError when File::metadata fails

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -5,4 +5,3 @@ use pyo3::exceptions::PyException;
 
 create_exception!(cramjam, CompressionError, PyException);
 create_exception!(cramjam, DecompressionError, PyException);
-create_exception!(cramjam, CramjamError, PyException);
