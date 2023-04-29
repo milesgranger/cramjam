b219f1cf00fa69ddc42a141aa371ddc8eb6923a6
Miles <miles59923@gmail.com>
2023-04-29 06:57:45 +0200

    Accept anything with Buffer Protocol, remove numpy (#100)
    - Adds support for anything which implements the buffer protocol
    - As a result, removes need for numpy and PyBytes and PyByteArray
      implementations in BytesType variants

--- src/lib.rs
+++ src/lib.rs
@@ -61,9 +61,10 @@ pub mod lz4;
 pub mod snappy;
 pub mod zstd;
 
+use io::{PythonBuffer, RustyBuffer};
 use pyo3::prelude::*;
 
-use crate::io::{AsBytes, RustyBuffer, RustyFile, RustyNumpyArray, RustyPyByteArray, RustyPyBytes};
+use crate::io::{AsBytes, RustyFile};
 use exceptions::{CompressionError, DecompressionError};
 use std::io::{Read, Seek, SeekFrom, Write};
 
@@ -72,34 +73,26 @@ use std::io::{Read, Seek, SeekFrom, Write};
 /// the documentation to see what types are acceptable for de/compression functions.
 #[derive(FromPyObject)]
 pub enum BytesType<'a> {
-    /// `bytes`
-    #[pyo3(transparent, annotation = "bytes")]
-    Bytes(RustyPyBytes<'a>),
-    /// `bytearray`
-    #[pyo3(transparent, annotation = "bytearray")]
-    ByteArray(RustyPyByteArray<'a>),
-    /// [`cramjam.File`](io/struct.RustyFile.html)
-    #[pyo3(transparent, annotation = "File")]
-    RustyFile(&'a PyCell<RustyFile>),
     /// [`cramjam.Buffer`](io/struct.RustyBuffer.html)
     #[pyo3(transparent, annotation = "Buffer")]
     RustyBuffer(&'a PyCell<RustyBuffer>),
-    /// `numpy.array` with `dtype=np.uint8`
-    #[pyo3(transparent, annotation = "numpy")]
-    NumpyArray(RustyNumpyArray<'a>),
+    /// [`cramjam.File`](io/struct.RustyFile.html)
+    #[pyo3(transparent, annotation = "File")]
+    RustyFile(&'a PyCell<RustyFile>),
+    /// `object` implementing the Buffer Protocol
+    #[pyo3(transparent, annotation = "pybuffer")]
+    PyBuffer(PythonBuffer<u8>),
 }
 
 impl<'a> AsBytes for BytesType<'a> {
     fn as_bytes(&self) -> &[u8] {
         match self {
-            BytesType::Bytes(b) => b.as_bytes(),
-            BytesType::ByteArray(b) => b.as_bytes(),
-            BytesType::NumpyArray(b) => b.as_bytes(),
             BytesType::RustyBuffer(b) => {
                 let py_ref = b.borrow();
                 let bytes = py_ref.as_bytes();
                 unsafe { std::slice::from_raw_parts(bytes.as_ptr(), bytes.len()) }
             }
+            BytesType::PyBuffer(b) => b.as_slice(),
             BytesType::RustyFile(b) => {
                 let py_ref = b.borrow();
                 let bytes = py_ref.as_bytes();
@@ -109,14 +102,12 @@ impl<'a> AsBytes for BytesType<'a> {
     }
     fn as_bytes_mut(&mut self) -> &mut [u8] {
         match self {
-            BytesType::Bytes(b) => b.as_bytes_mut(),
-            BytesType::ByteArray(b) => b.as_bytes_mut(),
-            BytesType::NumpyArray(b) => b.as_bytes_mut(),
             BytesType::RustyBuffer(b) => {
                 let mut py_ref = b.borrow_mut();
                 let bytes = py_ref.as_bytes_mut();
                 unsafe { std::slice::from_raw_parts_mut(bytes.as_mut_ptr(), bytes.len()) }
             }
+            BytesType::PyBuffer(b) => b.as_slice_mut().unwrap(),
             BytesType::RustyFile(b) => {
                 let mut py_ref = b.borrow_mut();
                 let bytes = py_ref.as_bytes_mut();
@@ -129,41 +120,35 @@ impl<'a> AsBytes for BytesType<'a> {
 impl<'a> Write for BytesType<'a> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         let result = match self {
-            BytesType::RustyFile(out) => out.borrow_mut().inner.write(buf)?,
             BytesType::RustyBuffer(out) => out.borrow_mut().inner.write(buf)?,
-            BytesType::ByteArray(out) => out.write(buf)?,
-            BytesType::NumpyArray(out) => out.write(buf)?,
-            BytesType::Bytes(out) => out.write(buf)?,
+            BytesType::RustyFile(out) => out.borrow_mut().inner.write(buf)?,
+            BytesType::PyBuffer(out) => out.write(buf)?,
         };
         Ok(result)
     }
     fn flush(&mut self) -> std::io::Result<()> {
         match self {
-            BytesType::RustyFile(f) => f.borrow_mut().flush(),
             BytesType::RustyBuffer(b) => b.borrow_mut().flush(),
-            BytesType::ByteArray(_) | BytesType::Bytes(_) | BytesType::NumpyArray(_) => Ok(()),
+            BytesType::RustyFile(f) => f.borrow_mut().flush(),
+            BytesType::PyBuffer(_) => Ok(()),
         }
     }
 }
 impl<'a> Read for BytesType<'a> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         match self {
-            BytesType::RustyFile(data) => data.borrow_mut().inner.read(buf),
             BytesType::RustyBuffer(data) => data.borrow_mut().inner.read(buf),
-            BytesType::ByteArray(data) => data.read(buf),
-            BytesType::NumpyArray(array) => array.read(buf),
-            BytesType::Bytes(data) => data.read(buf),
+            BytesType::RustyFile(data) => data.borrow_mut().inner.read(buf),
+            BytesType::PyBuffer(data) => data.read(buf),
         }
     }
 }
 impl<'a> Seek for BytesType<'a> {
     fn seek(&mut self, style: SeekFrom) -> std::io::Result<u64> {
         match self {
-            BytesType::RustyFile(f) => f.borrow_mut().inner.seek(style),
             BytesType::RustyBuffer(b) => b.borrow_mut().inner.seek(style),
-            BytesType::ByteArray(a) => a.seek(style),
-            BytesType::NumpyArray(a) => a.seek(style),
-            BytesType::Bytes(b) => b.seek(style),
+            BytesType::RustyFile(f) => f.borrow_mut().inner.seek(style),
+            BytesType::PyBuffer(buf) => buf.seek(style),
         }
     }
 }
@@ -171,18 +156,6 @@ impl<'a> Seek for BytesType<'a> {
 impl<'a> BytesType<'a> {
     fn len(&self) -> usize {
         self.as_bytes().len()
-    }
-}
-
-impl<'a> IntoPy<PyObject> for BytesType<'a> {
-    fn into_py(self, py: Python) -> PyObject {
-        match self {
-            Self::Bytes(bytes) => bytes.inner.into(),
-            Self::ByteArray(byte_array) => byte_array.inner.into(),
-            Self::RustyFile(file) => file.to_object(py),
-            Self::RustyBuffer(buffer) => buffer.into_py(py),
-            Self::NumpyArray(array) => array.to_object(py),
-        }
     }
 }
 
@@ -236,7 +209,7 @@ macro_rules! generic {
                             $py.allow_threads(|| {
                                 $op(f_in, &mut buf_out $(, $level)? )
                             })
-                        }
+                        },
                         _ => {
                             let bytes_out = $output.as_bytes_mut();
                             $py.allow_threads(|| {
