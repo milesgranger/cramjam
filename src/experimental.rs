2e6e379c16b043fa7d61526979fd9b4b7268ef82
Miles Granger <miles59923@gmail.com>
2024-09-24 13:59:46 +0200

    Add experimental igzip (#182)

--- src/experimental.rs
+++ src/experimental.rs
@@ -12,4 +12,11 @@ pub mod experimental {
     #[cfg(any(feature = "blosc2", feature = "blosc2-static", feature = "blosc2-shared"))]
     #[pymodule_export]
     use crate::blosc2::blosc2;
+
+    #[cfg(all(
+        any(feature = "igzip", feature = "igzip-static", feature = "igzip-shared"),
+        target_pointer_width = "64"
+    ))]
+    #[pymodule_export]
+    use crate::igzip::igzip;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,11 @@ pub mod bzip2;
 pub mod deflate;
 #[cfg(any(feature = "gzip", feature = "gzip-static", feature = "gzip-shared"))]
 pub mod gzip;
+#[cfg(all(
+    any(feature = "igzip", feature = "igzip-static", feature = "igzip-shared"),
+    target_pointer_width = "64"
+))]
+pub mod igzip;
 #[cfg(feature = "lz4")]
 pub mod lz4;
 #[cfg(feature = "snappy")]
